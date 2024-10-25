c8b3f2b24558dd64d37afccd1aef4e5bb755a085
Bayzman <bassmaninno102@gmail.com>
2024-10-25 13:12:22 -0700

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,6 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs """
-    
+    doc = mongo_collection.insert_one(kwargs)
+
+    return doc.inserted_id
